610f8926c50b100a7aef2993c57fda9fba67c6b1
Prathamesh  Thakare <prathamthackeray18@gmail.com>
2026-02-21 19:58:23 +0530

    add cors middleware

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const productRoute = require('./routes/product.route');
+const cors = require('cors');
 
 
 
 const app = express();
 
 app.use(express.json()); //to get the data from the req.body
+app.use(cors());
 
 app.use('/api/products',productRoute);
 // app.get('/api/products/get-item',productRoute.getItem);
