f9d22bdfc583fce9436652405dbe197df8829d4f
Prathamesh  Thakare <prathamthackeray18@gmail.com>
2026-02-21 13:38:42 +0530

    product model and update .env

---
+++ Backend/src/models/product.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+
+const productSchema = new mongoose.Schema({
+    image: String,
+    title: String,
+    description: String,
+    price: {
+        amount : {
+            type: Number,
+            min : 0
+        },
+        currency: {
+            type: String,
+            default: 'INR',
+            enum : ["INR", "USD"]
+        }
+    } ,
+    category: String
+})
+
+
+const productModel = mongoose.model('product', productSchema);
+
+module.exports = productModel;
